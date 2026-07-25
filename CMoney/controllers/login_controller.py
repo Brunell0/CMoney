1c9a1a418e7f108c747be22b80bc0b465e874279
JoaoTurolla <programadorjoaoturolla@gmail.com>
2026-07-25 18:34:22 -0300

    Início da reestruturação do login_controller, a função login() está melhor idealizada mas ainda incompleta e atrasada, será priorizada nas próximas atualizações

--- CMoney/controllers/login_controller.py
+++ CMoney/controllers/login_controller.py
@@ -1,1 +1,16 @@
-# Aqui será o controller de logins
+from models.db import Db
+from models.usuario import Usuario
+
+class LoginController: # precisa receber __dbC como os outros controllers
+    def __init__(self, db: Db):
+        self.__db = db
+        self.__usuarioAtual: Usuario = None
+
+    def login(self, username: str, senha: str) -> tuple[bool, str]: # incompleto 
+        # deverá devolver o nome do usuário para o sistema
+        user = self.__db.__usuarios.get(username) # encontra usuario pelo nome
+        if user and user.password == senha: # checa senha
+            self.__usuario_atual = user
+            self.registrar_log("LOGIN", f"Usuário '{username}' iniciou sessão.")
+            return True
+        return False
