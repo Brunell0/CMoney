d32ae678036f34fb416cbe35d2ad49d8a9be84c0
JoaoTurolla <programadorjoaoturolla@gmail.com>
2026-07-29 21:43:15 -0300

    Refatoração buscando remover acessos em cadeia de variáveis privadas e adição da função remover_usuário() para remover usuários a partir do nome

--- CMoney/controllers/Registration_controller.py
+++ CMoney/controllers/Registration_controller.py
@@ -1,13 +1,25 @@
 from models.usuario import Usuario
 from typing import Dict
-from models.db import Db
+from Db_controller import DbController
+from Log_controller import LogController
 
 class RegistrationController:
-    def __init__(self, db: Db):
-        self.__db = db
+    def __init__(self, dbController: DbController, logController: LogController):
+        self.__dbC = dbController
+        self.__lgC = logController
         
+    def registrar_usuario(self, username: str, senha: str, perfil: str) -> tuple[bool, str]:
+        if self.__dbC.checa_usuario_existe(username):
+            return False, "Este nome de usuário não está disponível"
+        else:
+            newUser = Usuario(username, senha, perfil)
+            self.__dbC.adiciona_usuario(username, newUser)
+            self.__lgC.registrar_log("CRIAR_USUÁRIO", f"Usuário: '{username}' criado", username)
+            self.__dbC.salvar_dados()
+            return True, f"Usuário: '{username}' criado com sucesso"
 
-    def registrar_usuario(self, username: str, senha: str, perfil: str) -> bool:
-            if username in self.__db.__usuarios:
-                return False
-            
+    def remover_usuario(self, userToRemove: str, nome_usuario: str):
+        if self.__dbC.checa_usuario_existe(userToRemove):
+            self.__dbC.remove_usuario(userToRemove)
+            self.__lgC.registrar_log("DELETAR_USUÁRIO", f"Usuário: '{userToRemove}' deletado", nome_usuario)
+            self.__dbC.salvar_dados()
