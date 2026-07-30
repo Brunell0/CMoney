3d967d3b5f0540d3bb7e293552dc709f45490574
JoaoTurolla <programadorjoaoturolla@gmail.com>
2026-07-29 21:45:03 -0300

    Refatoração buscando remover accesos em cadeia de variáveis privadas, garantir que os Logs recebam o nome do usuário que executou as ações e remoção de função duplicada atualizar_transacao()

--- CMoney/controllers/Transaction_controller.py
+++ CMoney/controllers/Transaction_controller.py
@@ -9,8 +9,9 @@ class TransactionController:
         self.__dbC = dbController
         self.proximo_id_transacao = 1
 
-    def criar_transacao(self, tipo: str, descricao: str, valor: float, categoria_nome: str) -> tuple:
-        cat = self.__dbC.__db.__categorias.get(categoria_nome)
+    def criar_transacao(self, tipo: str, descricao: str, valor: float,
+            categoria_nome: str, nome_usuario: str) -> tuple[bool, str]:
+        cat = self.__dbC.encontra_categoria(categoria_nome)
         if not cat:
             return False, "Categoria não existe."
             
@@ -19,8 +20,7 @@ class TransactionController:
                 return False, "ERRO: Verba bloqueada para esta categoria!"
             
             # Soma tudo o que já gastou nessa categoria específica
-            total_gasto = sum(t.valor for t in self.__dbC.__db.__transacoes.values() 
-                if t.__class__.__name__ == "Despesa" and t.categoria == categoria_nome)
+            total_gasto = self.calcula_total_gasto(categoria_nome)
             
             # Checa se o novo valor estoura o limite de verba
             if total_gasto + valor > cat.limite_verba:
@@ -35,60 +35,58 @@ class TransactionController:
             ) if tipo == "Receita" else Despesa(self.proximo_id_transacao, descricao, valor, categoria_nome)
 
 
-        self.__dbC.__db.__transacoes[t.id] = t
+        self.__dbC.adiciona_transacao(t.id, t)
         self.proximo_id_transacao += 1
         
-        # Repassa a para as funções dos outros controllers
-        self.__lgC.registrar_log("CRIAR_TRANSACAO", f"{tipo}: R${valor:.2f} em {categoria_nome} ({descricao}).")
+        self.__lgC.registrar_log("CRIAR_TRANSACAO", 
+            f"{tipo}: R${valor:.2f} em {categoria_nome} ({descricao}).", 
+            nome_usuario)
+        
         self.__dbC.salvar_dados()
 
         return True, "Transação criada com sucesso"
 
-    def atualizar_transacao(self, id_t: int, tipo: str, descricao: str, valor: float, categoria_nome: str) -> bool:
-        if id_t not in self.transacoes: return False
-        cat = self.__dbC.__db.__categorias.get(categoria_nome)
+    def atualizar_transacao(self, id_t: int, tipo: str, descricao: str, valor: float,
+            categoria_nome: str, nome_usuario: str) -> bool:
+        if self.__dbC.checa_transacao_existe(id_t): return False
+        cat = self.__dbC.encontra_categoria(categoria_nome)
         
         if tipo == "Despesa" and cat:
             if cat.bloqueada: return False
             
             # Soma o gasto dos outros registros (ignorando o que está sendo editado agora)
-            total_gasto = sum(t.valor for t in self.transacoes.values() if t.__class__.__name__ == "Despesa" 
-                and t.categoria == categoria_nome and t.id != id_t)
+            total_gasto = self.calcula_total_gasto(categoria_nome)
             
             if total_gasto + valor > cat.limite_verba:
                 return False # Vai disparar o bloco except/erro na View
 
         if tipo == "Receita":
             t = Receita(id_t, descricao, valor, categoria_nome)
-            self.__db.__transacoes[id_t] = t
+            self.__dbC.adiciona_transacao(id_t, t)
         elif tipo == "Despesa":
             t = Despesa(id_t, descricao, valor, categoria_nome)
-            self.__db.__transacoes[id_t] = t
+            self.__dbC.adiciona_transacao(id_t, t)
 
-        self.__lg.registrar_log("EDITAR_TRANSACAO", f"ID {id_t} alterado para {tipo} de R${valor:.2f} em {categoria_nome}.")
+        self.__lgC.registrar_log("EDITAR_TRANSACAO",
+            f"ID {id_t} alterado para {tipo} de R${valor:.2f} em {categoria_nome}.",
+            nome_usuario)
+        
         self.__dbC.salvar_dados()
 
         return True
-        
-    def listar_transacoes(self) -> List[Transacao]:
-        return list(self.transacoes.values())
 
-    def atualizar_transacao(self, id_t: int, tipo: str, descricao: str, valor: float, categoria_nome: str) -> bool:
-        if id_t not in self.transacoes: return False
-        t = Receita(id_t, descricao, valor, categoria_nome) if tipo == "Receita" else Despesa(id_t, descricao, valor, categoria_nome)
-        self.transacoes[id_t] = t
-        self.registrar_log("EDITAR_TRANSACAO", f"ID {id_t} alterado para {tipo} de R${valor:.2f} em {categoria_nome}.")
-        self.salvar_dados()
-        return True
-
-    def deletar_transacao(self, id_t: int) -> bool:
-        if id_t in self.transacoes:
-            t = self.transacoes[id_t]
-            self.registrar_log("DELETAR_TRANSACAO", f"Transação ID {id_t} ({t.descricao}) removida.")
-            del self.transacoes[id_t]
-            self.salvar_dados()
-            return True
-        return False
+    def deletar_transacao(self, id_t: int, nome_usuario: str) -> tuple[bool, str]:
+        if self.__dbC.checa_transacao_existe(id_t):
+            t = self.__dbC.encontra_transacao(id_t)
+            self.__lgC.registrar_log("DELETAR_TRANSACAO", f"Transação ID {id_t} ({t.descricao}) removida.", nome_usuario)
+            self.__dbC.remove_transacao(id_t)
+            self.__dbC.salvar_dados()
+            return True, f"Transação de id: '{id_t}' removida com sucesso"
+        return False, f"Nenhuma transação com id: '{id_t}' foi encontrada"
 
     def calcular_saldo(self) -> float:
-        return sum(t.calcular_impacto() for t in self.transacoes.values())
+        return sum(t.calcular_impacto() for t in self.__dbC.__db.__transacoes.values())
+
+    def calcula_total_gasto(self, cat_nome) -> float:
+       return sum(t.valor for t in self.__dbC.__db.__transacoes.values() 
+            if t.__class__.__name__ == "Despesa" and t.categoria == cat_nome)
